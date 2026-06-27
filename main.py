1237187d009fae5adc9810eacd3729e01ab12c05
Anjali Binu <anjali0.0binu@gmail.com>
2026-06-27 13:08:11 +0530

    Change API message from 'Congratulations' to 'Hello World'

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/get-message")
 async def read_root():
-  return {"Message" : "Congratulations on your first API"}
+  return {"Message" : "Hello World"}
